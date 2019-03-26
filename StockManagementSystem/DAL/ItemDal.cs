ddf3e32ac3e766b256cfb1d7713e4b99b591309d
Shakikul Islam <shakikulislam625010@gmail.com>
2019-03-26 15:15:29 +0600

    Create App.config Databsae Connection

--- StockManagementSystem/DAL/ItemDal.cs
+++ StockManagementSystem/DAL/ItemDal.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,11 @@ namespace StockManagementSystem.DAL
 {
     public class ItemDal
     {
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+        private static SqlConnection _sqlConnection = new SqlConnection(_connectionString);
+        private static SqlCommand _sqlCommand = new SqlCommand("", _sqlConnection);
+        private static SqlDataReader _sqlDataReader;
+        private static SqlDataAdapter _sqlDataAdapter;
+        private static DataTable _dataTable;
     }
 }
